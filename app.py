197dbd8fadebd667b77d63ee0b48c8e7e1f6820b
Avnish Kumar <vanish.kumar@vichara.com>
2025-10-31 20:50:01 +0530

    fix bug

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 Avnish Kumar
+# Licensed under the MIT License.
+# See LICENSE file in the project root for details.
+
 import streamlit as st
 from transformers import pipeline
 
